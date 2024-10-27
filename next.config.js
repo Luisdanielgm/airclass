583dee709d17a0c3a5fbcaecec4090f92fc86974
Luisdanielgm <luisdanielgm19@gmail.com>
2024-10-27 17:43:15 -0400

    first commit

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ['@mui/material', '@mui/system', '@emotion/react', '@emotion/styled']
+};
+
+module.exports = nextConfig;
+
