747e8fc2d2062de4a89ba27606c20530c4aa1af6
Luisdanielgm <luisdanielgm19@gmail.com>
2024-10-30 18:52:29 -0400

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,19 @@
-'use client'
-
 import localFont from "next/font/local";
 import "./globals.css";
 import { Sidebar } from '@/components/layout/Sidebar'
 import { LoadingProvider } from '@/components/providers/LoadingProvider'
+
+export const metadata = {
+  title: "Plataforma de Cursos de Aeronáutica",
+  description: "Plataforma educativa especializada en valuación de aeronaves",
+};
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
