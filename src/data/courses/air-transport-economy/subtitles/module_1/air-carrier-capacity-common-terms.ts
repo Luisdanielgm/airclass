0eff8446127823d691f596ab40bfaf752cc510d8
Luisdanielgm <luisdanielgm19@gmail.com>
2024-11-24 21:22:03 -0400

    update

--- src/data/courses/air-transport-economy/subtitles/module_1/air-carrier-capacity-common-terms.ts
+++ src/data/courses/air-transport-economy/subtitles/module_1/air-carrier-capacity-common-terms.ts
@@ -1,3 +1,10 @@
 export const airCarrierCapacityCommonTerms = [
-    { time: 0, text: "El módulo uno está basado en el manual sobre la regulación del transporte aéreo internacional doc nueve seis dos seis parte cuatro capítulo tres." }
+    { time: 0, text: "Algunos términos se utilizan comúnmente al discutir la capacidad de los transportistas aéreos. Haga clic en cada término para aprender más sobre él." },
+    { time: 9, text: "Factor de carga – El porcentaje de capacidad disponible que se vende y se utiliza realmente por pasajeros y/o carga en un vuelo único sobre un sector de vuelo único." },
+    { time: 20, text: "Factor de carga promedio. El factor de carga medio logrado durante un período de tiempo en un vuelo determinado, sector de vuelo, ruta, en un mercado particular, o por un transportista aéreo particular." },
+    { time: 36, text: "Factor de carga de equilibrio. El factor de carga en el que el ingreso logrado iguala el costo operativo, promediado para reflejar los resultados durante un período de tiempo especificado." },
+    { time: 46, text: "Capacidad Autorizada" },
+    { time: 49.5, text: "La cantidad de capacidad determinada por un estado o estados reguladores que puede ser operada en un vuelo o ruta específica entre pares de ciudades o entre dos estados." },
+    { time: 62, text: "Asignación de Capacidad" },
+    { time: 63.5, text: "La cantidad de capacidad que cada aerolínea está permitida para operar cuando más de una aerolínea designada de un estado desea utilizar la capacidad autorizada." },
 ]; 
